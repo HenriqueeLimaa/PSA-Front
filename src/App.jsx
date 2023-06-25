d8668f470f7dd43ae2af01139ed70d41c41e880e
Henrique Barcellos Lima <85499980+HenriqueeLimaa@users.noreply.github.com>
2023-06-25 20:21:59 -0300

    persiste o login

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,49 @@ import { SigninPage } from "./pages/signinPage/signinPage";
 import { AdminDashboard } from "./pages/adminDashboard/adminDashboard";
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("token") || ""
+  );
+  const [loginResponse, setLoginResponse] = useState("");
+  const [showLogin, setShowLogin] = useState(!localStorage.getItem("token"));
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
-    setIsAdmin(false);
+    setAccessToken("");
+    setShowLogin(true);
   };
 
   const loginHandler = () => {
+    // login info
     const loginInfo = {
       username: document.getElementById("emailInput").value,
       password: document.getElementById("passwordInput").value,
     };
 
+    // send a json with login info
     fetch("http://localhost:8081/api/auth/signin", {
       method: "POST",
       headers: {
+        "Content-Type": "application/json",
         Origin: "http://127.0.0.1:5173",
       },
-      body: loginInfo,
+      body: JSON.stringify(loginInfo),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setLoginResponse(data);
+        localStorage.setItem("token", data.accessToken);
+        setAccessToken(data.accessToken);
+        setShowLogin(false);
+      });
   };
 
   return (
     <div className="app-container">
-      <SigninPage onLogin={loginHandler} />
-      {/* <AdminDashboard /> */}
+      {showLogin && <SigninPage onLogin={loginHandler} />}
+      {!showLogin && (
+        <AdminDashboard token={accessToken} onLogout={logoutHandler} />
+      )}
     </div>
   );
 }
